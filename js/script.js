898aa983a6f434bf5dbd1318d845694c394b3410
Laura Simoni <laura.simoni@gmail.com>
2020-11-15 10:01:03 +0000

    define snakes' movement

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-snake[0] = {
+snake[0] ={
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function createBG(){
     context.fillStyle = "lightgreen";
@@ -19,5 +20,26 @@ function createSnake(){
     }
 }
 
-createBG();
-createSnake();
+function startGame(){
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    console.log("Direction: "+ direction + " x: " + snakeX + " y: " + snakeY);
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead); //método unshift adiciona como primeiro quadradinho da cobrinha
+    console.log("Direction: "+ direction + " x: " + snakeX + " y: " + snakeY);
+}
+
+let game = setInterval(startGame, 100);
